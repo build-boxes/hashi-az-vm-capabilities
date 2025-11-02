aae018bfbe890d2e1af32371661654c220f0a324
Hammad Rauf <rauf.hammad@gmail.com>
2025-11-02 13:20:22 -0500

    wip

--- terraform-provider-azvmcapability/provider/provider.go
+++ terraform-provider-azvmcapability/provider/provider.go
@@ -2,16 +2,17 @@ package provider
 
 import (
 	"context"
+
+	"github.com/hashicorp/terraform-plugin-framework/datasource"
 	"github.com/hashicorp/terraform-plugin-framework/provider"
 	"github.com/hashicorp/terraform-plugin-framework/provider/schema"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
-	"github.com/hashicorp/terraform-plugin-framework/datasource"
 )
 
 // Ensure the implementation satisfies the expected interfaces.
-// var (
-//     _ provider.Provider = &azvmcapabilityProvider{}
-// )
+var (
+	_ provider.Provider = &azvmcapabilityProvider{}
+)
 
 func New() provider.Provider {
 	return &azvmcapabilityProvider{}
@@ -27,7 +28,8 @@ func (p *azvmcapabilityProvider) Schema(_ context.Context, _ provider.SchemaRequ
 	resp.Schema = schema.Schema{}
 }
 
-func (p *azvmcapabilityProvider) Configure(ctx context.Context, req provider.ConfigureRequest, resp *provider.ConfigureResponse) {}
+func (p *azvmcapabilityProvider) Configure(ctx context.Context, req provider.ConfigureRequest, resp *provider.ConfigureResponse) {
+}
 
 func (p *azvmcapabilityProvider) DataSources(_ context.Context) []func() datasource.DataSource {
 	return []func() datasource.DataSource{
